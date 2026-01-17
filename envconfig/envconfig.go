c9eed825829d5ca034d797e9f78f2a57d0ea7259
rainbow777 <sky1111mountain@gmail.com>
2026-01-17 13:07:06 +0900

    Final sync: unify environment variables across envconfig and GitHub Actions

--- envconfig/envconfig.go
+++ envconfig/envconfig.go
@@ -19,8 +19,8 @@ var AppConfig EnvConfig
 
 func LoadEnvConfig() {
 	AppConfig = EnvConfig{
-		DBUser:         os.Getenv("USERNAME"),
-		DBPass:         os.Getenv("USERPASS"),
+		DBUser:         os.Getenv("DB_USER"),
+		DBPass:         os.Getenv("DB_PASS"),
 		DBName:         os.Getenv("DATABASE"),
 		DBHost:         os.Getenv("DBHOST"),
 		TestDBname:     os.Getenv("TESTDB"),
